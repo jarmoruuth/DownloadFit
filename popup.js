dcb31a5e93870231edbf8dc8c70b855bb05cdf65
jarmoruuth <jarmoruuth@users.noreply.github.com>
2020-01-20 23:50:37 +0200

    Fixes
    DownloadFit stopped working for some reason. Restructured the code and now it works again.

--- popup.js
+++ popup.js
@@ -2,15 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This extension demonstrates using chrome.downloads.download() to
-// download URLs.
+// This is a modified extension from a Chromium sample.
+// Extension downloads all .fit files from a page
 
-var allLinks = [];
-var visibleLinks = [];
+// Links to .fit files are saved here.
+// When we click download files are there from
+// here.
+var foundLinks = [];
 
 // Display all visible links.
-function showLinks() {
-  console.log('showLinks');
+function showLinks(visibleLinks) {
+  console.log('popup:showLinks');
   var linksTable = document.getElementById('links');
   while (linksTable.children.length > 1) {
     linksTable.removeChild(linksTable.children[linksTable.children.length - 1])
@@ -19,129 +21,110 @@ function showLinks() {
     var row = document.createElement('tr');
     var col0 = document.createElement('td');
     var col1 = document.createElement('td');
-    /*var checkbox = document.createElement('input');
-    checkbox.checked = true;
-    checkbox.type = 'checkbox';
-    checkbox.id = 'check' + i;
-    col0.appendChild(checkbox);*/
     col1.innerText = visibleLinks[i];
     col1.style.whiteSpace = 'nowrap';
-    /*col1.onclick = function() {
-      checkbox.checked = !checkbox.checked;
-    }*/
-    //row.appendChild(col0);
     row.appendChild(col1);
     linksTable.appendChild(row);
   }
 }
 
-// Toggle the checked state of all visible links.
-function toggleAll() {
-  var checked = document.getElementById('toggle_all').checked;
-  for (var i = 0; i < visibleLinks.length; ++i) {
-    document.getElementById('check' + i).checked = checked;
-  }
-}
-
-// Download all visible checked links.
+// Download all links.
 function downloadCheckedLinks() {
-  console.log('downloadCheckedLinks');
+  console.log('popup:downloadCheckedLinks');
+  var visibleLinks = foundLinks;
+  console.log('popup:downloadCheckedLinks:visibleLinks.length', visibleLinks.length);
   for (var i = 0; i < visibleLinks.length; ++i) {
-    //if (document.getElementById('check' + i).checked) {
       console.log(visibleLinks[i]);
-      chrome.downloads.download({url: visibleLinks[i]},
-                                             function(id) {
-                                              console.log('download');
-                                            });
-    //}
+      chrome.downloads.download(
+        {
+            url: visibleLinks[i]
+        },
+        function(id) {
+          console.log('popup:downloadCheckedLinks:download', id);
+        }
+      );
   }
   // If we close the window then files are not saved
   //window.close();
 }
 
-// Re-filter allLinks into visibleLinks and reshow visibleLinks.
-function filterLinks() {
-  console.log('filterLinks');
-
-  /*var filterValue = document.getElementById('filter').value;
-  if (document.getElementById('regex').checked) {
-    visibleLinks = allLinks.filter(function(link) {
-      return link.match(filterValue);
-    });
-  } else*/ {
-    var terms = filterValue.split(' ');
-    visibleLinks = allLinks.filter(function(link) {
-      for (var termI = 0; termI < terms.length; ++termI) {
-        var term = terms[termI];
-        if (term.length != 0) {
-          var expected = (term[0] != '-');
-          if (!expected) {
-            term = term.substr(1);
-            if (term.length == 0) {
-              continue;
-            }
-          }
-          var found = (-1 !== link.indexOf(term));
-          if (found != expected) {
-            return false;
-          }
-        }
-      }
-      return true;
-    });
-  }
-  showLinks();
-}
-
-// Add links to allLinks and visibleLinks, sort and show them.  send_links.js is
-// injected into all frames of the active tab, so this listener may be called
-// multiple times.
-chrome.extension.onRequest.addListener(function(links) {
-  console.log('addListener');
+// Process links, sort, remove duplicates and show them.  
+function process_links(links) {
+  console.log('popup:process_links:links.length', links.length);
+  var allLinks = [];
   for (var index in links) {
     // Check if .fit link is part or url parameter
-    console.log('addListener: link ' + links[index]);
+    console.log('popup:process_links: link ' + links[index]);
     var parts = links[index].split("?");
     for (var i = 0; i < parts.length; i++) {
       if (parts[i].indexOf('.fit') != -1) {
-        console.log('addListener: keep link part ' + parts[i]);
+        console.log('popup:process_links: keep link part ' + parts[i]);
         if (parts[i].indexOf('url=') != -1) {
           parts[i] = parts[i].substr(4);
         }
         allLinks.push(parts[i]);
       } else {
-        console.log('addListener: skip link part ' + parts[i]);
+        console.log('popup:process_links: skip link part ' + parts[i]);
       }
     }
   }
+
+  console.log('popup:sort:allLinks.length', allLinks.length);
   allLinks.sort();
+
   // remove duplicates
-  for (var i = 1; i < allLinks.length;) {
-    if (allLinks[i-1] == allLinks[i]) {
-      links.splice(i, 1);
-    } else {
-      i++;
+  console.log('popup:remove duplicates');
+  var finalLinks = [];
+  if (allLinks.length > 0) {
+    finalLinks.push(allLinks[0]);
+    for (var i = 1; i < allLinks.length; i++) {
+      if (allLinks[i-1] != allLinks[i]) {
+        finalLinks.push(allLinks[i]);
+      }
     }
   }
-  visibleLinks = allLinks;
-  showLinks();
-});
+  
+  console.log('popup:done:finalLinks.length', finalLinks.length);
+  
+  showLinks(finalLinks);
+  
+  return finalLinks;
+}
 
 // Set up event handlers and inject send_links.js into all frames in the active
 // tab.
-window.onload = function() {
-  //document.getElementById('filter').onkeyup = filterLinks;
-  //document.getElementById('regex').onchange = filterLinks;
-  //document.getElementById('toggle_all').onchange = toggleAll;
-  document.getElementById('download0').onclick = downloadCheckedLinks;
-  //document.getElementById('download1').onclick = downloadCheckedLinks;
+window.onload = function() 
+{
+    document.getElementById('download0').onclick = downloadCheckedLinks;
 
-  chrome.windows.getCurrent(function (currentWindow) {
-    console.log('getCurrent');
-    chrome.tabs.query({active: true, windowId: currentWindow.id},
-                      function(activeTabs) {
-      chrome.tabs.executeScript(
-        activeTabs[0].id, {file: 'send_links.js', allFrames: true});
+    chrome.windows.getCurrent(function (currentWindow) {
+          chrome.tabs.query(
+              {
+                  active: true, 
+                  windowId: currentWindow.id
+              },
+              function(activeTabs) {
+                console.log('popup:executeScript');
+                chrome.tabs.executeScript(
+                      activeTabs[0].id, 
+                      {
+                          file: 'send_links.js', 
+                          allFrames: true
+                      },
+                      function (results) {
+                          // Process results. There may be multiple frames
+                          // but for now we assume that only one of them has
+                          // .fit files.
+                          for (var i = 0; i < results.length; i++) {
+                              console.log('popup:executeScript:results', i, results[i]);
+                              if (results[i].length > 0) {
+                                console.log('popup:executeScript:foundLinks set');
+                                foundLinks = process_links(results[i]);
+                              }
+                          }
+                      }
+                  );
+              }
+          );
     });
-  });
 };
--- send_links.js
+++ send_links.js
@@ -2,38 +2,42 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// This is a modified extension from a Chromium sample.
 // Send back to the popup a sorted deduped list of valid link URLs on this page.
 // The popup injects this script into all frames in the active tab.
 
-var links = [].slice.apply(document.getElementsByTagName('a'));
-links = links.map(function(element) {
-  // Return an anchor's href attribute, stripping any URL fragment (hash '#').
-  // If the html specifies a relative path, chrome converts it to an absolute
-  // URL.
-  var href = element.href;
-  var hashIndex = href.indexOf('#');
-  if (hashIndex >= 0) {
-    href = href.substr(0, hashIndex);
-  }
-  return href;
-});
+(function () {
 
-links.sort();
+  var links = [].slice.apply(document.getElementsByTagName('a'));
+  links = links.map(function(element) {
+    // Return an anchor's href attribute, stripping any URL fragment (hash '#').
+    // If the html specifies a relative path, chrome converts it to an absolute
+    // URL.
+    var href = element.href;
+    var hashIndex = href.indexOf('#');
+    if (hashIndex >= 0) {
+      href = href.substr(0, hashIndex);
+    }
+    return href;
+  });
 
-// Remove duplicates and invalid URLs.
-var kBadPrefix = 'javascript';
-for (var i = 0; i < links.length;) {
-  if (((i > 0) && (links[i].trim() == links[i - 1].trim())) ||
-      (links[i] == '') ||
-      (kBadPrefix == links[i].toLowerCase().substr(0, kBadPrefix.length)) ||
-      links[i].indexOf('.fit') == -1) 
+  links.sort();
+
+  // Remove duplicates and invalid URLs.
+  var kBadPrefix = 'javascript';
+  var finalLinks = [];
+  for (var i = 0; i < links.length; i++) {
+    if (((i > 0) && (links[i].trim() == links[i - 1].trim())) ||
+        (links[i] == '') ||
+        (kBadPrefix == links[i].toLowerCase().substr(0, kBadPrefix.length)) ||
+        links[i].indexOf('.fit') == -1) 
     {
-        console.log('skip link ' + i + ' ' + links[i]);
-        links.splice(i, 1);
-  } else {
-    console.log('keep link ' + i + ' ' + links[i]);
-    ++i;
+      console.log('send_links:skip link ' + i + ' ' + links[i]);
+    } else {
+      console.log('send_links:keep link ' + i + ' ' + links[i]);
+      finalLinks.push(links[i]);
+    }
   }
-}
+  return finalLinks;
 
-chrome.extension.sendRequest(links);
+})();
